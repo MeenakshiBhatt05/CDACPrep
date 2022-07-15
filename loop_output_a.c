8a76ec1f11dde882e4a439b980879f0865af0ca8
meenakshibhatt05 <meenakshibhatt05@gmail.com>
2022-07-15 18:09:05 +0530

    loop output and switch complete

---
+++ loop_output_a.c
@@ -0,0 +1,92 @@
+/*#include <stdio.h>
+int main()
+{
+    int i = 1;
+    while (i <= 10)
+        ;
+    {
+        printf("%d\n", i);
+        i++;
+    }
+    return 0;
+}*/
+
+/*#include <stdio.h>
+int main()
+{
+    int x = 4;
+    while (x == 1)
+    {
+        x = x - 1;
+        printf("%d\n", x);
+        --x;
+    }
+    return 0;
+}*/
+
+/*#include <stdio.h>
+int main()
+{
+    int x = 4, y, z;
+    y = --x;
+    z = x--;
+    printf("%d %d %d\n", x, y, z);
+    return 0;
+}*/
+
+/*#include <stdio.h>
+int main()
+{
+    int x = 4, y = 3, z;
+    z = x-- - y;
+    printf("%d %d %d\n", x, y, z);
+    return 0;
+}*/
+
+/*#include <stdio.h>
+int main()
+{
+    while ('a' < 'b')
+        printf("malyalam is palindrome\n");
+    return 0;
+}*/
+
+/*#include <stdio.h>
+int main()
+{
+    int i;
+    while (i = 10)
+    {
+        printf("%d\n", i);
+        i = i + 1;
+    }
+    return 0;
+}*/
+
+/*#include <stdio.h>
+int main()
+{
+    float x = 1.1;
+    while (x = 1.1)
+    {
+        printf("%f\n", x);
+        x = x - .1;
+    }
+    return 0;
+}*/
+
+#include <stdio.h>
+int main()
+{
+    int x = 4, y = 0, z;
+    while (x >= 0)
+    {
+        x--;
+        y++;
+        if (x == y)
+            continue;
+        else
+            printf("\n%d %d", x, y);
+    }
+    return 0;
+}

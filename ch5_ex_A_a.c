cf41eb96e0d75a64bf44a7a886ec258ad44863d4
NitishAndola <nitish.andola@gmail.com>
2022-07-20 00:09:17 +0530

    function_test

---
+++ ch5_ex_A_a.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+void display();
+int main()
+{
+    printf("\nOnly Stupids use c?");
+    display();
+    return 0;
+}
+
+void display()
+{
+    printf("\n Fools too use C!");
+    main();
+}
